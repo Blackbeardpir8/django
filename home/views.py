5c87ec31174b3d9a71a5e6823a4e1a7d5bf1a41d
Blackbeardpir8 <deepakdk15yadav@gmail.com>
2024-07-27 22:14:53 +0530

    vo changes ko hata rha to check git sahi se kaam kr rha ki nahi

--- home/views.py
+++ home/views.py
@@ -75,8 +75,4 @@ def search_page(request):
     context = {'students' : students,'search':search }
     return render(request , 'search.html',context)
 
-"""def deepak():
-    KeyboardInterruptsdfsldkflsdf
-    sdkflksdflksdjlfl
-    sdfjsldkfjlksdf
-    sdhflsdkjflskjfj"""
+"
