fa14923b86b7df23612609e439d2d9cf1bce8fad
Blackbeardpir8 <deepakdk15yadav@gmail.com>
2024-07-31 11:53:25 +0530

    aggregate price min

--- scripts.py
+++ scripts.py
@@ -10,13 +10,14 @@ import random
 from faker import Faker
 from home.models import Author, Book
 from datetime import datetime, timedelta
-from django.db.models import Sum 
+from django.db.models import Sum , Min ,Max
 
 
 def handle():
     #books = Book.objects.Sum()
     #books = Book.objects.aggregate(price = Avg('price'))
-    books = Book.objects.aggregate(price = Sum('price'))
+    books = Book.objects.aggregate(price = Min('price'))
+    
     print(books)
 
 handle()
