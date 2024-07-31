5e34e8cbf6932988a1a3846b8ae6ff71724eda0a
Blackbeardpir8 <deepakdk15yadav@gmail.com>
2024-07-31 11:51:05 +0530

    aggregate Books Sum

--- scripts.py
+++ scripts.py
@@ -10,11 +10,13 @@ import random
 from faker import Faker
 from home.models import Author, Book
 from datetime import datetime, timedelta
-from django.db.models import Avg 
+from django.db.models import Sum 
 
 
 def handle():
-    books = Book.objects.aggregate(price = Avg('price'))
+    #books = Book.objects.Sum()
+    #books = Book.objects.aggregate(price = Avg('price'))
+    books = Book.objects.aggregate(price = Sum('price'))
     print(books)
 
 handle()
