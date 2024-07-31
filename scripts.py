a868138f3402472141821af445238aad2ce85da2
Blackbeardpir8 <deepakdk15yadav@gmail.com>
2024-07-31 13:05:39 +0530

    annotate using multiple annotate total earning and bookcount

--- scripts.py
+++ scripts.py
@@ -15,10 +15,11 @@ from django.db.models import Sum , Min ,Max,Count,Avg,Q
 
 def handle():
     authors = Author.objects.annotate(
-       total_earning=Sum('book__price',)
-    )
+       total_earning=Sum('book__price'),
+       book_count_2023 = Count('book' , filter=Q(book__published_date__year = 2023))
+        )
     
     for author in authors:
-        print(f"Author name = {author.author_name}, earning = {author.total_earning}")
+        print(f"Author name = {author.author_name}, earning = {author.total_earning} Book Count {author.book_count_2023} ")
 
 handle()
