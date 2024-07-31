c39ac5a91ae9a5d5eaf3fbc03e8097ea1f8c6094
Blackbeardpir8 <deepakdk15yadav@gmail.com>
2024-07-31 12:41:06 +0530

    annotate authors with book more than 50

--- scripts.py
+++ scripts.py
@@ -15,7 +15,7 @@ from django.db.models import Sum , Min ,Max,Count,Avg,Q
 
 def handle():
     authors = Author.objects.annotate(
-        book_count = Count('book',filter =Q(book__published_date__year__gte = 2023))
+        book_count = Count('book',filter =Q(book__price__gte = 50))
         )
     for author in authors:
         print(f"Author name = {author.author_name} Book count {author.book_count}")
