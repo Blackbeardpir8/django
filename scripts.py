e6e0edc62d945f7b91115047483bd0aacc16ba4d
Blackbeardpir8 <deepakdk15yadav@gmail.com>
2024-07-31 11:47:16 +0530

    aggregate sum

---
+++ scripts.py
@@ -0,0 +1,20 @@
+import os
+
+import django
+import itertools
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'firstproject.settings'
+django.setup()
+import django
+import random
+from faker import Faker
+from home.models import Author, Book
+from datetime import datetime, timedelta
+from django.db.models import Avg 
+
+
+def handle():
+    books = Book.objects.count()
+    print(books)
+
+handle()
