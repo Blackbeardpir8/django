4c9cf85ad87c80a68bca8e6752ad14738e3dddfb
Blackbeardpir8 <deepakdk15yadav@gmail.com>
2024-07-31 12:50:17 +0530

    annotate earning of all authors

--- scripts.py
+++ scripts.py
@@ -15,10 +15,10 @@ from django.db.models import Sum , Min ,Max,Count,Avg,Q
 
 def handle():
     authors = Author.objects.annotate(
-        book_count=Count('book', filter=Q(book__price__gte=50))
-    ).filter(book_count__gte=1)
+       total_earning=Sum('book__price',)
+    )
     
     for author in authors:
-        print(f"Author name = {author.author_name}, Book count = {author.book_count}")
+        print(f"Author name = {author.author_name}, earning = {author.total_earning}")
 
 handle()
