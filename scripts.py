6c7de59c12c68532fd3cc61a573e111e0cdef614
Blackbeardpir8 <deepakdk15yadav@gmail.com>
2024-07-31 12:24:18 +0530

    annotate total author total books and max price

--- scripts.py
+++ scripts.py
@@ -15,10 +15,10 @@ from django.db.models import Sum , Min ,Max,Count,Avg
 
 def handle():
     #authors = Author.objects.annotate(total_books = Count('book'))
-    authors = Author.objects.annotate(avg_price = Avg('book__price'),total_books = Count('book'), min_price= Min('book__price'))
+    authors = Author.objects.annotate(avg_price = Avg('book__price'),total_books = Count('book'), max_price= Max('book__price'))
     
     for author in authors:
-        print(f"Min price = {author.min_price} Author name {author.author_name} total book {author.total_books} total avg price {author.avg_price}")
+        print(f"max price = {author.max_price} Author name {author.author_name} total book {author.total_books} total avg price {author.avg_price}")
     
 
 handle()
