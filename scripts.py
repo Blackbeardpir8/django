5ae8c836919ae068352dd8c1d10dbea9d0c613eb
Blackbeardpir8 <deepakdk15yadav@gmail.com>
2024-08-01 11:47:21 +0530

    for loop bulk update  method

--- scripts.py
+++ scripts.py
@@ -17,16 +17,22 @@ from home.models import *
 fake = Faker()
 
 
-def createPerson(number):
+"""def createPerson(number):
     create = [Person(person_name = fake.name()) for i in range (number)]
-    Person.objects.bulk_create(create)
+    Person.objects.bulk_create(create)"""
 
 
-    
-#def deletePerson(number):
-#        Person.objects.all.delete()#
+
+"""def deletePerson(number):
+       Person.objects.all().delete()"""
+
+
+def updatePerson(name):
+    print(Person.objects.filter(person_name__icontains = name).count())
+    print(Person.objects.filter(person_name__icontains = name).update(person_name = "Watson"))
+
+updatePerson("Dennis")
 
 #deletePerson(1000)
 
-
-createPerson(100000)
+#createPerson(10000000)
